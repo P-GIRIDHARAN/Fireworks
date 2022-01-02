9b8a6dbf1594c6e01ed55e2aa8451721f46c22e3
GIRIDHARAN P <70087687+P-GIRIDHARAN@users.noreply.github.com>
2022-01-02 21:02:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,33 +1,92 @@
-var fireworks = [];
-var gravity;
-var fsound;
-function preload() {
-  fsound = loadSound("Fireworksound.mp3");
-}
-
-function setup() {
-  createCanvas(1600, 720);
-  colorMode(HSB);
-  gravity = createVector(0, 0.2);
-  stroke(255);
-  strokeWeight(8);
-  background(0);
-}
-
-function draw() {
-  colorMode(RGB);
-  background(0, 0, 0, 25);
-  if (random(1) < 0.02) {
-    fireworks.push(new Firework());
+function Particle(x, y, hu, firewor) {
+  this.pos = createVector(x, y);
+  this.firewor = firewor;
+  this.lifespan = 255;
+  this.hu = hu;
+  if (this.firewor) {
+    this.vel = createVector(0, random(-16, -8));
+  } else {
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(random(2, 15));
   }
-  for (var i = fireworks.length - 1; i >= 0; i--) {
-    fireworks[i].update();
-    fireworks[i].show();
-    if (fireworks[i].done()) {
-      fireworks.splice(i, 1);
+  this.acc = createVector(0, 0);
+  this.applyForce = function (force) {
+    this.acc.add(force);
+  };
+  this.update = function () {
+    if (!this.firewor) {
+      this.vel.mult(0.95);
+      this.lifespan -= 5;
     }
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+
+    this.done = function () {
+      if (this.lifespan < 0) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+  };
+  this.show = function () {
+    colorMode(HSB);
+    if (!this.firewor) {
+      strokeWeight(4);
+      stroke(hu, 255, 255, this.lifespan);
+    } else {
+      strokeWeight(8);
+      stroke(hu, 255, 255);
+    }
+    point(this.pos.x, this.pos.y);
+    if (mouseIsPressed === true) {
+      point(mouseX, mouseY);
+    }
+  };
+}
+function ParticleM(x, y, hu, firewor) {
+  this.pos = createVector(x, y);
+  this.firewor = firewor;
+  this.lifespan = 255;
+  this.hu = hu;
+  if (this.firewor) {
+    this.vel = createVector(0, -16);
+  } else {
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(random(2, 15));
   }
+  this.acc = createVector(0, 0);
+  this.applyForce = function (force) {
+    this.acc.add(force);
+  };
+  this.update = function () {
+    if (!this.firewor) {
+      this.vel.mult(0.95);
+      this.lifespan -= 5;
+    }
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(-0.01);
+
+    this.done = function () {
+      if (this.lifespan < 0) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+  };
+  this.show = function () {
+    colorMode(HSB);
+    if (!this.firewor) {
+      strokeWeight(4);
+      stroke(hu, 255, 255, this.lifespan);
+    } else {
+      strokeWeight(8);
+      stroke(hu, 255, 255);
+    }
+    point(this.pos.x, this.pos.y);
+  };
 }
-function mouseClicked() {
-  fireworks.push(new FireworkM());
-}
+
