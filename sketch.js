755bf771cf8b679dea8b4701c5f3fc0e6f9bb97c
GIRIDHARAN P <70087687+P-GIRIDHARAN@users.noreply.github.com>
2022-01-03 20:08:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ var gravity;
 var fsound;
 function preload() {
   fsound = loadSound("Fireworksound.mp3");
+  frameRate(90);
 }
 
 function setup() {
@@ -12,6 +13,7 @@ function setup() {
   stroke(255);
   strokeWeight(8);
   background(0);
+  frameRate(90);
 }
 
 function draw() {
@@ -19,15 +21,18 @@ function draw() {
   background(0, 0, 0, 25);
   if (random(1) < 0.02) {
     fireworks.push(new Firework());
+    frameRate(90);
   }
   for (var i = fireworks.length - 1; i >= 0; i--) {
     fireworks[i].update();
     fireworks[i].show();
     if (fireworks[i].done()) {
       fireworks.splice(i, 1);
+      frameRate(90);
     }
   }
 }
 function mouseClicked() {
   fireworks.push(new FireworkM(mouseX, mouseY));
+  frameRate(90);
 }
